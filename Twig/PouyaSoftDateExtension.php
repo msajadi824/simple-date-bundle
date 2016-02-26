614312d90c8d887fc4b846c43ce4f7baeb51c9ed
MOSTAFA-PC <msajadi824@gotmail.com>
2016-02-26 20:07:53 +0330

    fix

--- Twig/PouyaSoftDateExtension.php
+++ Twig/PouyaSoftDateExtension.php
@@ -17,7 +17,8 @@ class PouyaSoftDateExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('jSDate', array($this, 'jSDateFilter'))
+            new \Twig_SimpleFilter('jSDate', array($this, 'jSDateFilter')),
+            new \Twig_SimpleFilter('jSDayWeek', array($this, 'jSDayWeekFilter'))
         );
     }
 
@@ -29,6 +30,15 @@ class PouyaSoftDateExtension extends \Twig_Extension
     public function jSDateFilter($date, $separator = null)
     {
         return $this->jDateService->MiladiToShamsi($date, $separator);
+    }
+
+    /**
+     * @param \DateTime $date
+     * @return string
+     */
+    public function jSDayWeekFilter($date)
+    {
+        return $this->jDateService->getWeekDay($date);
     }
 
     public function getName()
