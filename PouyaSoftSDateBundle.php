9382df3a0d7b1b98a55437a446153d6fcf8c958d
Mostafa Sajadi <msajadi824@gmail.com>
2024-09-16 22:42:18 +0330

    update to symfony 7

--- PouyaSoftSDateBundle.php
+++ PouyaSoftSDateBundle.php
@@ -2,8 +2,13 @@
 
 namespace PouyaSoft\SDateBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class PouyaSoftSDateBundle extends Bundle
+class PouyaSoftSDateBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // load an XML, PHP or YAML file
+        $container->import('../config/services.yml');
+    }
 }
---
+++ config/bundles.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    PouyaSoft\SDateBundle\PouyaSoftSDateBundle::class => ['all' => true],
+];
