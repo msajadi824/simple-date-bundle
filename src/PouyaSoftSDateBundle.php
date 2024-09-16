9528cfcdfe6e533b48c113fdab5c490a52ed04a2
Mostafa Sajadi <msajadi824@gmail.com>
2024-09-16 23:54:24 +0330

    .

--- src/PouyaSoftSDateBundle.php
+++ src/PouyaSoftSDateBundle.php
@@ -2,13 +2,15 @@
 
 namespace PouyaSoft\SDateBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 class PouyaSoftSDateBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         // load an XML, PHP or YAML file
-        $container->import('config/services.yml');
+        $container->import('../config/services.yml');
     }
 }
