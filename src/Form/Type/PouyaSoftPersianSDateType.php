0fb5c4623014d38c25f5e935f78a7688f9e45353
Mostafa Sajadi <msajadi824@gmail.com>
2024-09-17 12:31:47 +0330

    .

--- src/Form/Type/PouyaSoftPersianSDateType.php
+++ src/Form/Type/PouyaSoftPersianSDateType.php
@@ -1,49 +1,33 @@
 <?php
 namespace PouyaSoft\SDateBundle\Form\Type;
 
-use PouyaSoft\SDateBundle\Form\DataTransformer\PouyaSoftSDateTransformer;
-use PouyaSoft\SDateBundle\Service\jDateService;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PouyaSoftPersianSDateType extends AbstractType
 {
-    /**
-     * {@inheritdoc}
-     */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $view->vars['pickerOptions'] = $options['pickerOptions'];
     }
 
-    /**
-     * @param OptionsResolver $resolver
-     */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'pickerOptions' => [],
-        ));
+        ]);
 
         $resolver->setAllowedTypes('pickerOptions', ['array', 'null']);
     }
 
-    /**
-     * @return string
-     */
     public function getParent()
     {
-        return 'Symfony\Component\Form\Extension\Core\Type\TextType';
+        return TextType::class;
     }
 
-    /**
-     * @return string
-     */
     public function getBlockPrefix()
     {
         return 'persianSDate';
--- src/Form/Type/PouyaSoftSDateType.php
+++ src/Form/Type/PouyaSoftSDateType.php
@@ -4,19 +4,16 @@ namespace PouyaSoft\SDateBundle\Form\Type;
 use PouyaSoft\SDateBundle\Form\DataTransformer\PouyaSoftSDateTransformer;
 use PouyaSoft\SDateBundle\Service\jDateService;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PouyaSoftSDateType extends AbstractType
 {
-    /** @var jDateService */
     private $jDateService;
-
-    /** @var string */
     protected $locale;
 
     public function __construct(jDateService $jDateService, RequestStack $requestStack)
@@ -25,36 +22,26 @@ class PouyaSoftSDateType extends AbstractType
         $this->locale = $requestStack->getCurrentRequest()->getLocale() == 'fa' ? 'fa' : 'en';
     }
 
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $transformer = new PouyaSoftSDateTransformer($this->jDateService, $options['serverFormat'], $options['locale']);
         $builder->addModelTransformer($transformer);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $view->vars['pickerOptions'] = $options['pickerOptions'];
         $view->vars['locale'] = $options['locale'];
     }
 
-    /**
-     * @param OptionsResolver $resolver
-     */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'invalid_message' => $this->locale == 'fa' ? 'تاریخ وارد شده اشتباه است' : 'Selected date is invalid.',
             'serverFormat' => 'yyyy/MM/dd',
             'locale' => $this->locale,
             'pickerOptions' => [],
-        ));
+        ]);
 
         $resolver->setAllowedTypes('serverFormat', ['string', 'null']);
         $resolver->setAllowedTypes('locale', ['string', 'null']);
@@ -62,35 +49,13 @@ class PouyaSoftSDateType extends AbstractType
         $resolver->setAllowedTypes('pickerOptions', ['array', 'null']);
     }
 
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $this->configureOptions($resolver);
-    }
-
-    /**
-     * @return string
-     */
     public function getParent()
     {
-        return 'Symfony\Component\Form\Extension\Core\Type\TextType';
+        return TextType::class;
     }
 
-    /**
-     * @return string
-     */
     public function getBlockPrefix()
     {
         return 'jSDate';
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->getBlockPrefix();
     }
 } 
